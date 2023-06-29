8022d9de18f6b9649b6b08d13a16e491f27eddc9
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-06-29 22:30:16 +0800

    latest update

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -24,6 +24,28 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
               className="object-contain ml-4"
             />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            Leave="transition ease-in duration-100"
+            LeaveFrom="opacity-100"
+            LeaveTo="opacity-10"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={() =>
+                    `relative px-4 py-2 cursor-default select-none ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected }) => <span>{option.title}</span>}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
